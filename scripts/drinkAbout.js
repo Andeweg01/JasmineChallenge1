ffa297c8dbc19e9b7c523191d722548bf0e1ad43
Sascha Andeweg <sascha@designed.irish>
2020-01-18 20:36:14 +0000

    recommit to push a working test set into github pages

--- scripts/drinkAbout.js
+++ scripts/drinkAbout.js
@@ -1,6 +1,6 @@
 whatCanIDrink = function(age) {
     if (age < 0) {
-        return "Sorry. I can’t tell what drink because that age is incorrect!";
+        return "Sorry. I can't tell what drink because that age is incorrect!";
     }
     else if (age < 14) {
         return "Drink Toddy";
